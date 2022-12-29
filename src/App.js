925fed6ae9a4d7148af0008f6cca9969c42289a7
Anant-Singh1 <97743499+Anant-Singh1@users.noreply.github.com>
2022-12-29 11:13:09 +0530

    Corrected the weather icon code

--- src/App.js
+++ src/App.js
@@ -4,23 +4,6 @@ import "./App.css";
 import City from "./Components/City";
 import Weather from "./Components/Weather";
 
-export const WeatherIcon = {
-  "01d": "../Assets/icons/sunny.svg",
-  "01n": "../Assets/icons/night.svg",
-  "02d": "../Assets/icons/day.svg",
-  "02n": "../Assets/icons/cloudy-night.svg",
-  "03d": "../Assets/icons/cloudy.svg",
-  "03n": "../Assets/icons/cloudy.svg",
-  "04d": "../Assets/icons/perfect-day.svg",
-  "04n": "../Assets/icons/cloudy-night.svg",
-  "09d": "../Assets/icons/rain.svg",
-  "09n": "../Assets/icons/rain-night.svg",
-  "10d": "../Assets/icons/rain.svg",
-  "10n": "../Assets/icons/rain-night.svg",
-  "11d": "../Assets/icons/storm.svg",
-  "11n": "../Assets/icons/storm.svg",
-};
-
 const API_KEY = "f8a070560de031ed38f1f9dc043d6cb1";
 
 const App = () => {
--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Weather.css";
-// import weatherlogo from "../Assets/icons/perfect-day.svg";
-// import infoicon from "../Assets/icons/temperature.svg";
+
 import sunrise from "../Assets/icons/sunrise.svg";
 import sunset from "../Assets/icons/sunset.svg";
 import humidity from "../Assets/icons/humidity.svg";
 import wind from "../Assets/icons/wind.svg";
 import pressure from "../Assets/icons/pressure.svg";
-import { WeatherIcon } from "../App";
+
+import cloudynight from "../Assets/icons/cloudy-night.svg";
+import sunny from "../Assets/icons/sunny.svg";
+import night from "../Assets/icons/sunny.svg";
+import cloudy from "../Assets/icons/cloudy.svg";
+import day from "../Assets/icons/day.svg";
+import rain from "../Assets/icons/rain.svg";
+import rainnight from "../Assets/icons/rain-night.svg";
+import storm from "../Assets/icons/storm.svg";
+import perfectday from "../Assets/icons/perfect-day.svg";
 
 const WeatherInfo = ({ name, value, icon }) => {
   return (
@@ -22,32 +30,35 @@ const WeatherInfo = ({ name, value, icon }) => {
 };
 
 const Weather = ({ weather }) => {
-  const isDay = weather?.weather[0].icon.includes("d");
-  const getTime = (timeStamp) => {
-    return `${new Date(timeStamp * 1000).getHours()} : ${new Date(
-      timeStamp * 1000
-    ).getMinutes()}`;
+  const getIcon = () => {
+    const wth = weather.weather[0].icon;
+    if (wth === "01d") return sunny;
+    else if (wth === "01n") return night;
+    else if (wth === "02d") return day;
+    else if (wth === "02n" || wth === "04n") return cloudynight;
+    else if (wth === "03d" || wth === "03n") return cloudy;
+    else if (wth === "04d") return perfectday;
+    else if (wth === "09d" || wth === "10d") return rain;
+    else if (wth === "09n" || wth === "10n") return rainnight;
+    else return storm;
   };
+  const isDay = weather?.weather[0].icon.includes("d");
 
   return (
     <>
       <div className="weather">
         <span className="condition">
-          <span>{`${Math.floor(weather.main.temp - 273)} C`}</span>
-          {`| ${weather.weather[0].description}`}
+          <span>{`${Math.floor(weather.main.temp - 273)}℃`}</span>
+          {` | ${weather.weather[0].description}`}
         </span>
-        <img
-          src={WeatherIcon[weather.weather[0].icon]}
-          className="weatherlogo"
-          alt="All...day"
-        />
+        <img src={getIcon()} className="weather-logo" alt="All...day" />
       </div>
       <span className="location">{`${weather.name},${weather.sys.country}`}</span>
       <span className="weather-info-label">Weather Info</span>
       <div className="weather-info-container">
         <WeatherInfo
           name={isDay ? "Sunset" : "Sunrise"}
-          value={getTime(weather.sys[isDay ? "Sunset" : "Sunrise"])}
+          // value={getTime(weather.sys[isDay ? "Sunset" : "Sunrise"])}
           icon={isDay ? sunset : sunrise}
         />
         <WeatherInfo name="wind" value={weather.wind.speed} icon={wind} />
